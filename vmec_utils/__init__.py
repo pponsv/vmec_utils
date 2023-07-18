17d3f33ff671de9a8ae5e53068363e304e9e87ba
Pedro <ponsvpedro@gmail.com>
2023-07-18 16:02:08 +0200

    Reorganized __init__.py

--- vmec_utils/__init__.py
+++ vmec_utils/__init__.py
@@ -1,9 +1,7 @@
-# from .commands import *
 from .class_booz import Booz
 from .class_vmec import Vmec
-from .helper import *
+from . import helper
 
-# from .fn import *
 from . import fn
 from . import commands
 from .plot import Plotter
