8e13f2e1c08c719a28f2f1f7459ff9c9bd04bb4c
Pedro <ponsvpedro@gmail.com>
2023-10-23 17:49:02 +0200

    Added integral functions

--- vmec_utils/fn.py
+++ vmec_utils/fn.py
@@ -4,6 +4,41 @@ import numpy as np
 from time import time
 
 
+def delta_vec(vec):
+    nvec = np.zeros(len(vec))
+    nvec[0] = (vec[1] - vec[0]) / 2
+    nvec[-1] = (vec[-1] - vec[-2]) / 2
+    for i in range(1, len(vec) - 1):
+        nvec[i] = (vec[i + 1] - vec[i - 1]) / 2
+    return nvec
+
+
+def vol_integral(e1, e2, e3, jac, quantity):
+    de1 = delta_vec(e1)
+    de2 = delta_vec(e2)
+    de3 = delta_vec(e3)
+    # jac = vmec.vars["sqrtg_vecs"]
+    # njac = vmec.vars["sqrt_g"]
+    vol = 0
+    for i, de1i in enumerate(de1):
+        for j, de2i in enumerate(de2):
+            for k, de3i in enumerate(de3):
+                vol += de1i * de2i * de3i * jac[i, j, k] * quantity[i, j, k]
+    return vol
+
+
+def surf_integral(e1, e2, jac, quantity):
+    de1 = delta_vec(e1)
+    de2 = delta_vec(e2)
+    # jac = vmec.vars["sqrtg_vecs"]
+    # njac = vmec.vars["sqrt_g"]
+    vol = 0
+    for i, de1i in enumerate(de1):
+        for j, de2i in enumerate(de2):
+            vol += de1i * de2i * jac[i, j] * quantity[i, j]
+    return vol
+
+
 def get_extension(folderpath):
     inputfile = glob.glob(f"{folderpath}/input.*")
     extension = os.path.basename(inputfile[0])[6:]
