4ea17e400162be0899a5695092f2384f02be598f
pponsv <ponsvpedro@gmail.com>
2026-05-08 14:56:07 +0200

    fix coefs for low number of points

--- vmec_utils/utils.py
+++ vmec_utils/utils.py
@@ -27,16 +27,20 @@ def plot_rationals(ax, mnum=8, mden=8):
 
 def make_coef_array(coefs, xm, xn, len_th, len_ph, deriv_order=0, deriv_dir=""):
     """Make a 3D array of coefficients with the correct shape to be inverted."""
+    assert deriv_dir in ["th", "ph", ""], (
+        "Invalid derivative dir"
+    )  # "" means no derivative
     coef_array = np.zeros((coefs.shape[0], len_th, len_ph), dtype=np.complex128)
+    xm_mod = np.array(xm) % len_th
+    xn_mod = -np.array(xn) % len_ph
     if deriv_order == 0:
-        coef_array[:, xm, -xn] = coefs
+        coefs_mod = coefs
     elif deriv_order == 1:
         if deriv_dir == "th":
-            coef_array[:, xm, -xn] = 1j * xm * coefs
-        elif deriv_dir == "zt":
-            coef_array[:, xm, -xn] = -1j * xn * coefs
-        else:
-            raise ValueError("Invalid deriv_dir")
+            coefs_mod = 1j * xm * coefs
+        elif deriv_dir == "ph":
+            coefs_mod = -1j * xn * coefs
+    np.add.at(coef_array, np.s_[:, xm_mod, xn_mod], coefs_mod)
     return coef_array
 
 
